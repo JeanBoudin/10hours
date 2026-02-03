e253f1f12101c16524119dfd801dd2e89ebbe6f4
m.vaugoyeau <m.vaugoyeau@cicd.biz>
2026-02-03 14:56:46 +0100

    build fonctionnel

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)]
-            if let Some(window) = app.get_webview_window("main") {
-                window.open_devtools();
-                window.close_devtools();
+            {
+                use tauri::Manager;
+                if let Some(window) = _app.get_webview_window("main") {
+                    window.open_devtools();
+                    window.close_devtools();
+                }
             }
             Ok(())
         })
